e806d3cf7e99f0f58a2cc4d0ad5ac0926c6794fe
Ben MacBook <info@van-markruudnew.jordancap.com>
2014-06-16 07:39:34 -0700

    Resize script added, working to add products

---
+++ admin/add-products.php
@@ -0,0 +1,55 @@
+<?php include('../inc/dbconnect.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Add a Product</title>
+  <!-- Bootstrap -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesnt work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head> 
+<body>
+<div class="container">
+  <div class="page-header">
+   <div class="btn-group" style="float: right;">
+    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Admin Controls <span class="caret"></span></button>
+      <ul class="dropdown-menu" role="menu">
+        <li><a href="add-clients.php">Add Clients</a></li>
+        <li><a href="view-clients.php">View Clients</a></li>
+        <li class="divider"></li>
+        <li><a href="add-products.php">Add Products</a></li>
+        <li><a href="view-products.php">View Products</a></li>
+        <li class="divider"></li>
+        <li><a href="add-transactions.php">Add Transactions</a></li>
+        <li><a href="view-transactions.php">View Transactions</a></li>
+        <li class="divider"></li>
+        <li><a href="../inc/logout.php">Logout</a></li>
+      </ul>
+  </div>
+  <h3>Add a Product</h3> 
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plug</a>ins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ inc/resize.php
@@ -0,0 +1,38 @@
+<?php
+$uploadFile = basename($_FILES['uploadme']['name']);
+if (!empty($uploadFile)) {
+	$tempFile = $_FILES['uploadme']['tmp_name'];
+	$src = imagecreatefromjpeg($tempFile);
+
+	//Large file is 400px Wide
+	list($width,$height) = getimagesize($tempFile);
+	$newwidth = 400;
+	$newheight = ($height/$width)*400;
+	$tmp = imagecreatetruecolor($newwidth, $newheight);
+	imagecopyresampled($tmp, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height)
+
+	chdir('../products');
+	$uploadFolder = 'large/';
+	$uploadPath = $uploadFolder . $uploadFile;
+
+	$filename = $uploadFolder . $_Files['uploadme']['name'];
+	imagejpeg($tmp, $filename, 100);
+
+	//Small File is 100px Wide
+	list($width,$height)=getimagesize($tmpFile);
+	$newwidth = 100;
+	$newheight = ($height/$width)*100;
+	$tmp=imagecreatetruecolor($newwidth,$newheight);
+	imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,$width,$height);
+
+	chdir('../products');
+	$uploadFolder = 'small/'
+	$uploadPath = $uploadFolder . $uploadFile;
+
+	$filename = $uploadFolder . $_FILES['uploadme']['name'];
+	imagejpeg($tmp, $filename, 100);
+
+	imagedestroy($src);
+	imagedestroy($tmp);
+} 
+?>
